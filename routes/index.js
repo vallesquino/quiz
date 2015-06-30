256367526997192fb9ad09b25982b218bd2878e3
Pedro Vallesquino <pjvallesquino@financialforce.com>
2015-06-30 18:48:57 +0200

    ADD: Autoload

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var router = express.Router();
+
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
 router.get('/author',  function(req, res) {
   res.render('author');
 });
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
-
 module.exports = router;
+
+
+
