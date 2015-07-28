1c7d26a0fccd3a3bf7c0bbf363af306e8e322cae
Pedro Vallesquino <pjvallesquino@financialforce.com>
2015-07-28 16:42:01 +0200

    Revert f028d3d..194d5a9
    This rolls back to commit f028d3d68061f28ad68608b2ae553372d6b1f21e.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz', errors: []});
+  res.render('index', { title: 'Quiz' });
 });
 
 router.get('/author',  function(req, res) {
@@ -16,14 +16,9 @@ router.get('/author',  function(req, res) {
 router.param('quizId', quizController.load);  // autoload :quizId
 
 // Definición de rutas de /quizes
-router.get('/quizes',                      quizController.index);
+router.get('/quizes',                      quizController.search);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new',                  quizController.new);
-router.post('/quizes/create',              quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
-router.put('/quizes/:quizId(\\d+)',        quizController.update);
-router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
 module.exports = router;
 
