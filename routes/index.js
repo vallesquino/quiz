f04ca41a023909a5c9c354a7fbfebaf376625012
Pedro Vallesquino <pjvallesquino@financialforce.com>
2015-07-28 19:43:44 +0200

    FIX:autor

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/author',  function(req, res) {
-  res.render('author');
+  res.render('author', {errors: []});
 });
 
 // Autoload de comandos con :quizId
