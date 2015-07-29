d161e489c0bade3ceb172f16eaf5a87705c0b654
Pedro Vallesquino <pjvallesquino@financialforce.com>
2015-07-29 22:36:17 +0200

    FIX: style FIX: tema en update

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -82,6 +82,7 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
   req.quiz.pregunta  = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
   req.quiz
   .validate()
